51f2340c40a8845fe09bfeeb88196cd29f598628
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-04-09 11:17:24 +0530

    sum

---
+++ s.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	int a,b,sum=0;
+	printf("enter two numbers:");
+	scanf("%d" "%d",&a,&b);
+	sum=a+b;
+	printf("the sum of %d and %d is %d\n",a,b,sum);
+	return 0;
+}
