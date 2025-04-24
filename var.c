2f13c17e70fe53483f0f43af38cc47a76954bffd
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-04-24 19:56:29 +0530

    variable

---
+++ var.c
@@ -0,0 +1,41 @@
+<html>
+<head>
+</head>
+<body>
+<script>
+function area(){
+var a=10;
+var c=a*a;
+document.write(c);
+}
+</script>
+<p id ="my"></p>
+<button onclick ="area()">Area</button>
+<script>
+function add(){
+var a=10;
+var b=20;
+var c=a+b;
+document.write(c);
+}
+</script>
+<p id ="my"></p>
+<button onclick ="add()">add</button>
+<script>
+function sub(){
+var a=10;
+var b=20;
+var c=a-b;
+document.write(c);
+}
+</script>
+<p id ="my"></p>
+<button onclick ="sub()">sub</button>
+<script>
+function div(){
+var a=10;
+var b=20;
+var c=a/b;
+document.write(c);
+}
+
