b9dc9871da966f204c1b0e25ae9f97044eb58e96
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-04-24 20:15:27 +0530

    print

---
+++ print.c
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2> the window.print() method<h2>
+<p> click the button to print the current page .</p>
+<button onclick ="window.print()">print this page</button>
+</body>
+</html>
