235bd1443fc6ec735d5ffb8f11d5a6201e3e2839
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-04-25 13:08:36 +0530

    CHOICE

--- choice.c
+++ choice.c
@@ -4,4 +4,16 @@ int main() {
     int choice;
     float num1, num2, result;
 
+ printf("Simple Calculator\n");
+    printf("------------------\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
 
